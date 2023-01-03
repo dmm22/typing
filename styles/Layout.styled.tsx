9d908f1e28d895665298bf52c553f113447d811c
dmm22 <davidmartin@dgaauto.com>
2023-01-03 15:03:39 -0500

    Add styles to center text block

--- styles/Layout.styled.tsx
+++ styles/Layout.styled.tsx
@@ -2,4 +2,8 @@ import styled from "styled-components"
 
 export const LayoutContainer = styled.div`
   margin: 0 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
